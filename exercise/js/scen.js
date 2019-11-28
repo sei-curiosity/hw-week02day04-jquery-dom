f53bf09beef4353228a6681331efe9653fad6d2c
gneral20 <gneral20@gmail.com>
2019-11-28 08:50:48 +0300

    The Best Homework Ever

---
+++ exercise/js/scen.js
@@ -0,0 +1,39 @@
+$(function(){
+    $(".wrapper h2").css({
+        color:"red",
+
+    })
+})
+
+$(function(){
+    $("blockquote").css({
+        color: "white",
+        backgroundColor: "black",
+        border: "1px solid black",
+        fontWeight: "bold",
+        textAlign: "center",
+        padding: "10px 50px",
+        borderRadius: "8px"
+    })
+})
+
+$(function(){
+    $(".title").text("This text had been Changed by JavaScript ")
+    $(".title").css({
+        color:"red",
+        fontWeight:"bold",
+        textDecoration: "underline",
+    })
+    $(".falseFact").css({
+        fontFamily: "Helvetica",
+        color: "green"
+    })
+
+})
+$(function(){
+    $(".wrapper p").css({
+        border: "1px solid black",
+        padding: "10px",
+        borderRadius: "5px",
+    })
+})
