d17c5a5326405bbf95e1a57b1ae4cc2be3dd3e73
ooeid <ooeid.omar@gmail.com>
2019-11-28 08:43:29 +0300

    myhomework

--- exercise/js/main.js
+++ exercise/js/main.js
@@ -1,108 +1,95 @@
-/**
- * Javascript/jQuery Exercises: Selecting Things In The DOM!
-* Answer the following questions. This practice set gets progressively harder.
-* Remember to test each question, before moving on. Once you're achieved the desired outcome, comment out the code and 
-* move on to the next question.
- */
-
-////////////////////////////////////////////////////////////////////////////////
-// SECTION 1: REPLACE THE SECTIONS THAT SAY REPLACE ME WITH THE CORRECT CODE. //
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * Question 1:
- * When, button 1 is clicked, select all paragraphs on the page with jQuery, and changed the font color to blue.
- * We've started you off, ONLY replace everything instance of "replaceMe" with correct code.
- */
+$(function(){
+    $('h1').css('color','#802b00')
+    $('.p1').css('color','teal');
+    $('h2').css('color','blue');
+    $('.p0').css('color','red');
+    $('.p2').css('color','green');
+    $('li').css('color','rgb(255, 153, 102)');
+    $('.p4').css('color','rgb(128, 96, 0)' , "font-family", "Arial, Helvetica, sans-serif");
+    $('blockqoute').css(' background','rgb(255, 198, 26)');
+})
 
 function question1() {
-	// your answer here
-}
+	$('p').css('color','blue');
 
+
+
+
+}
 $("#button1").on('click', question1);
 
-/**
- * Question 2:
- * When button 2 is clicked, select all <h2> elements on this page, and change the text to say your name.
- * Uncomment the code below and solve the problem
- */
-
 function question2(){
-	// your answer here
+$('h2').text('OMAR')
 }
 
 $("#button2").on('click', question2);
 
-/**
- * Question 3:
- * When button 3 is clicked, select the ordered list item that has the class "falseFact".
- * Using jQuery change the content to read "True Fact".
- * Uncomment the code below and solve the problem
- */
 
 function question3(){
-	// your answer here
+	$('.falseFact').text("TrueFact");
 }
 
 $("#button3").on('click', question3);
 
-///////////////////////////////////////////////////////////////////////////////////////////////
-// SECTION 2: Now you are on your own. Write the necessary code to accomplish the following. //
-///////////////////////////////////////////////////////////////////////////////////////////////
 
-/**
- * Question 4:
- * When button 4 is clicked, change the background color of the whole page to "pink"
- */
+function question4(){
+	$('body').css('background','pink')
+}
 
-// your answer here
+$("#button4").on('click', question4);
 
-/**
- * Question 5:
- * When button 5 is clicked, change the color of all h2's to "green"
- */
 
-// your answer here
+function question5(){
+	$('h2').css('color','green');
+}
 
-/**
- * Question 6:
- * When button 6 is clicked, change the html content of all "blockquotes" to be "<span>no quote</span>"
- */
+$("#button5").on('click', question5);
 
- // your answer here
 
-///////////////////////////////////////////////////////////////////////////////////////
-// SECTION 3: This section is more difficult. Try your hand at the next 3 questions. //
-///////////////////////////////////////////////////////////////////////////////////////
 
-/**
- * Question 7:
- * When button 7 is clicked, change the text in the header's h1 tag to read "jQuery Ninja"
- */
+function question6(){
+    $('blockquotes').html('<span>no quote</span>')
+    
+}
 
- // your answer here
+$("#button6").on('click', question6);
 
-/**
- * Question 8:
- * Have the following code execute when button 8 is clicked.
- * $(".city").attr("src", "http://lorempixel.com/g/500/400/food");
- */
 
- // your answer here
 
-/**
- * Question 9:
- * When button 9 is clicked, select all paragraphs on the page and change the color to blue and font to Georgia.
- */
+function question7(){
+	$('header h1').text('jQuery Ninja')
+}
 
-// your answer here
 
-/**
- * Question 10:
- * When button 10 is clicked, select all paragraphs on the page and change the color to blue and font to Georgia.
- * but with only one jQuery. $("selector").css({attribute: "value", attribute: "value"})
- */
+$("#button7").on('click', question7);
 
-// your answer here
+
+function question8(){
+    $(".city").attr("src", "http://lorempixel.com/g/500/400/food");
+}
+
+$("#button8").on('click', question8);
+
+
+
+function question9(){
+    $('p').css('color','blue');
+    $('p').css('fontWieght','Georgia')
+}
+
+$("#button9").on('click', question9);
+
+function question10(){
+
+    $('p').css({
+        color:'blue',
+        "font-weight":'Georgia'
+
+
+    })
+    
+}
+
+$("#button10").on('click', question10);
 
 
